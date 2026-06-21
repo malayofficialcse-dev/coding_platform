8c27053b62cf3d2c36f7737eb7a64d0035daa9bc
malayofficialcse-dev <malay.official.cse@gmail.com>
2026-06-21 22:29:18 +0530

    Fix MongoDB connection string in db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect( process.env.MONGO_URI || "mongodb://maitymalay334_db_user:dbmalay21mongo@ac-m8w7vpt-shard-00-00.vvosns0.mongodb.net:27017,ac-m8w7vpt-shard-00-01.vvosns0.mongodb.net:27017,ac-m8w7vpt-shard-00-02.vvosns0.mongodb.net:27017/?ssl=true&authSource=admin&retryWrites=true&w=majority");
+    await mongoose.connect(  process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error(err.message);
